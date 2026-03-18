ea1c491cdbe1291c963cb7fc2d10c2dd738c4990
dritisai <dritisaisivarathri@gmail.com>
2026-03-18 10:37:15 +0530

    added

--- src/com/example_project/Application2.java
+++ src/com/example_project/Application2.java
@@ -1,6 +1,6 @@
 package com.example_project;
 
-public class Application2 {
+public class Application2 extends Application{
 
 	public static void main(String[] args) {
 		System.out.println("Logout feature project");
